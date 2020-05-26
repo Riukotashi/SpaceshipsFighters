49dcbb658f7ed888f88643fd8aab9d2715d55691
kevin <kevgym69@gmail.com>
2020-05-26 16:09:37 +0200

    insertion en base de donnee de l historique de la partie

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
+using Mono.Data.Sqlite;
+
+
 
 public class Game : MonoBehaviour
 {
@@ -19,6 +23,7 @@ public class Game : MonoBehaviour
                 winner = player2.name;
                 loser = player1.name;
                 Debug.Log("winner is" + winner);
+                SaveScore(winner, loser);
                 end++;
             }
             if (player2.activeSelf == false)
@@ -27,14 +32,38 @@ public class Game : MonoBehaviour
                 winner = player1.name;
                 loser = player2.name;
                 Debug.Log("winner is" + winner);
+                SaveScore(winner, loser);
                 end++;
             }
         }
     }
 
-    void SaveScore()
+    void SaveScore(string winner, string loser)
     {
-        
+        // Create database
+        string connection = "URI=file:" + Application.persistentDataPath + "/SpaceshipsFigthersScore";
+        Debug.Log("path de la bdd sqlite " + Application.persistentDataPath);
+
+        // Open connection
+        IDbConnection dbcon = new SqliteConnection(connection);
+        dbcon.Open();
+
+        // Create table
+        IDbCommand dbcmd;
+        dbcmd = dbcon.CreateCommand();
+        string q_createTable = "CREATE TABLE IF NOT EXISTS gameHisto (id INTEGER PRIMARY KEY, winner TEXT, loser TEXT)";
+
+        dbcmd.CommandText = q_createTable;
+        dbcmd.ExecuteReader();
+
+        // Insert values in table
+        IDbCommand insert = dbcon.CreateCommand();
+        insert.CommandText = "INSERT INTO gameHisto (winner,loser) VALUES (@winner, @loser)";
+        insert.Parameters.Add(new SqliteParameter("@winner", winner));
+        insert.Parameters.Add(new SqliteParameter("@loser", loser));
+        insert.ExecuteNonQuery();
+        dbcon.Close();
+
 
     }
 }
